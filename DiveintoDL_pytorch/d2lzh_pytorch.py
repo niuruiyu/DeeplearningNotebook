fc281670ccd9b6e5d38f72a7cdfd2f90cda5b1e2
niuruiyu <1801844018@qq.com>
2025-09-01 23:14:38 +0800

    更新了一些小点

--- DiveintoDL_pytorch/d2lzh_pytorch.py
+++ DiveintoDL_pytorch/d2lzh_pytorch.py
@@ -3,6 +3,7 @@ import torch
 from IPython import display
 from matplotlib import pyplot as plt
 import random
+from torch import nn
 def use_svg_display():  
     backend_inline.set_matplotlib_formats('svg')  
 
@@ -46,3 +47,47 @@ def evaluate_accuracy(data_iter,net):
         acc_sum += (net(X).argmax(dim=1)==y).float().sum()
         n += y.shape[0]#获取当前批次的样本数
     return acc_sum/n
+
+class FlattenLayer(nn.Module):
+    def __init__(self):
+        super(FlattenLayer, self).__init__()
+    def forward(self, x): # x shape: (batch, *, *, ...)
+        return x.view(x.shape[0], -1)
+
+
+#训练的函数
+def train_ch3(net,train_iter,test_iter,loss,num_epochs,batch_size,params=None,lr =None,optimizer=None):
+    for epoch in range(num_epochs):
+        train_loss_sum,train_acc_sum,n=0.0,0.0,0
+        for X,y in train_iter:
+            y_hat = net(X)
+            l = loss(y_hat,y).sum()
+            #先梯度清零
+            if optimizer is not None:
+                optimizer.zero_grad()
+            elif params is not None and params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+            #反向传播
+            l.backward()
+            if optimizer is None:
+                sgd(params,lr,batch_size)
+            else:
+                optimizer.step()
+            train_loss_sum+=l
+            train_acc_sum+=(y_hat.argmax(dim=1) == y).sum().item()
+            n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter,net)
+        print('epoch %d,loss%.4f,train acc %.3f,test acc %.3f'
+              %(epoch+1,train_loss_sum/n,train_acc_sum/n,test_acc))
+
+
+def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
+             legend=None, figsize=(3.5, 2.5)):
+    set_figsize(figsize)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.semilogy(x_vals, y_vals)
+    if x2_vals and y2_vals:
+        plt.semilogy(x2_vals, y2_vals, linestyle=':')
+        plt.legend(legend)
